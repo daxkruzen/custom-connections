c7655135a71e9da8e8b2a4f3a77c808f5c4b3f31
daxkruzen <daxkruzen@icloud.com>
2025-04-22 13:48:41 -0600

    Update examples.ts

--- lib/examples.ts
+++ lib/examples.ts
@@ -2,10 +2,10 @@ import { GameOptions } from "./game-options";
 
 export const examples: GameOptions[] = [
   {
-    title: "#1",
+    title: "#3",
     author: "Dax Kruzen Pettibone",
     names: [
-      "WORDS FOR HORSE",
+      "THINGS",
       "OPPOSE",
       "PALINDROMES",
       "KEY___",
@@ -34,19 +34,19 @@ export const examples: GameOptions[] = [
     ],
   },
   {
-    title: "#3",
+    title: "#1",
     author: "Dax Kruzen Pettibone",
     names: [
-      "WORDS FOR HORSE",
-      "OPPOSE",
-      "PALINDROMES",
-      "KEY___",
+      "THINGS THAT MOVE SLOWLY",
+      "TOILET CLEANING METHODS",
+      "___ ISLAND(S)",
+      "INSURANCE COMPANY MASCOTS",
     ],
     words: [
-      ["MUSTANG", "MARE", "COLT", "BRONCO"],
-      ["BLOCK", "CHALLENGE", "RESIST", "COMBAT"],
-      ["CIVIC", "LEVEL", "ROTOR", "REFER"],
-      ["RING", "BOARD", "CHAIN", "NOTE"],
+      ["TURTLE", "MOLASIS", "SNAIL", "GLACIER"],
+      ["SNAKE", "PLUNGE", "FLUSH", "SCRUB"],
+      ["COOK", "MARSHALL", "EASTER", "VIRGIN"],
+      ["GECKO", "EMU", "DUCK", "GENERAL"],
     ],
   },
 ];
